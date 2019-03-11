5962da32ab32710e88ac423768e3d1cacf6e158a
Duncan Macleod <duncan.macleod@ligo.org>
2019-03-11 20:35:39 +0000

    gwosc.timeline: added another test

--- gwosc/tests/test_timeline.py
+++ gwosc/tests/test_timeline.py
@@ -48,6 +48,10 @@ def test_get_segments(flag, start, end, result):
     assert timeline.get_segments(flag, start, end) == result
 
 
+def test_get_segments_long():
+    assert len(timeline.get_segments('H1_DATA', 1126051217, 1137196817)) == 654
+
+
 @pytest.mark.remote
 def test_timeline_url():
     # check that unknown IFO results in no matches
