053913b620086566ccb655f199c663c2850791a6
Duncan Macleod <duncan.macleod@ligo.org>
2021-05-10 10:17:50 +0100

    update copyright/license header for all modules

--- gwosc/__init__.py
+++ gwosc/__init__.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) Cardiff University, 2018-2020
-#
-# This file is part of GWOSC.
-#
-# GWOSC is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# GWOSC is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GWOSC.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) Cardiff University (2018-2021)
+# SPDX-License-Identifier: MIT
 
+"""GWOSC: a python interface to the GW Open Science data archive
+"""
 
 __author__ = "Duncan Macleod <duncan.macleod@ligo.org>"
 __license = "MIT"
